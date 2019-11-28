9a903a2d63d803a2fd8522c9ff3cd254547bfcf4
saqib abbas <saqib4cs@gmail.com>
2019-11-28 21:27:07 +0500

    auth provider and auth guard added on authentication

---
+++ src/utils/authGuard.js
@@ -0,0 +1,9 @@
+import { AUTH_TOKEN, TOKEN_EXPIRY } from '../constants';
+
+class authGuard {
+    isAuthenticated() {
+        return localStorage.getItem(AUTH_TOKEN) && localStorage.getItem(TOKEN_EXPIRY) > new Date().toISOString();
+    }
+}
+
+export default new authGuard();
---
+++ src/utils/authProvider.js
@@ -0,0 +1,38 @@
+import httpClient from './httpClient';
+import { AUTH_TOKEN, TOKEN_EXPIRY } from '../constants';
+
+class authProvider {
+    async login(credentials) {
+        try {
+            // un-comment the http call once integrated with actual login API
+            //const response = await httpClient.post('login', credentials);
+            // set token and token expiry in local storage or as per your approach
+
+            //localStorage.setItem(AUTH_TOKEN, response.result.token);
+            //localStorage.setItem(TOKEN_EXPIRY, response.result.tokenExpiry);
+
+            // genenating token 2 minute token expiry time
+            const tokenExpiry = this.getTokenFakeExpiryTime(2).toISOString();
+
+            localStorage.setItem(AUTH_TOKEN, 'token');
+            localStorage.setItem(TOKEN_EXPIRY, tokenExpiry);
+            return Promise.resolve();
+        }
+        catch (error) {
+            return Promise.reject(error.message);
+        }
+    }
+
+    logout() {
+        localStorage.removeItem(AUTH_TOKEN);
+    }
+
+    refreshToken() {
+    }
+    getTokenFakeExpiryTime(minutes) {
+        let dt = new Date();
+        return new Date(dt.setUTCMinutes(dt.getUTCMinutes() + minutes));
+    }
+}
+
+export default new authProvider();
