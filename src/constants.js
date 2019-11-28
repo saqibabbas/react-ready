ad87de5eccc1f6f7b0e64f4d306aefd7c1b8c402
saqib abbas <saqib4cs@gmail.com>
2019-11-28 13:27:32 +0500

    axios updates

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+//http 
+export const HttpStatusCode = {
+    OK: 200,
+    BadRequest: 400,
+    Unauthorized: 401,
+    Forbidden: 403,
+    NotFound: 404,
+    InternalServerError: 500
+};
+export const NetworkError = 'Network Error. Try again later.';
+
--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
-
+import { HttpStatusCode, NetworkError } from '../constants';
 class httpClient {
     constructor() {
         //TODO: need to implement base url and version from enviornment variables
-        let service = axios.create({
+        let client = axios.create({
             baseURL: "http://www.mocky.io/v2/",
             responseType: "json"
         });
-        service.interceptors.request.use(this.handleRequest);
-        service.interceptors.response.use(this.handleSuccess, this.handleError);
-        this.service = service;
+        client.interceptors.request.use(this.handleRequest);
+        client.interceptors.response.use(this.handleSuccess, this.handleError);
+        this.client = client;
     }
     handleRequest(config) {
         // here we can add custom logic and headers in request e.g : Authorization header
@@ -20,32 +20,49 @@ class httpClient {
     }
 
     handleError = (error) => {
+        const customError = {
+            message: null,
+            status: null
+        };
         if (error.response) {
+            customError.status = error.response.status;
+            customError.message = error.response.statusText
             switch (error.response.status) {
-                case 401:
-                    this.redirectTo(document, '/notfound')
+                case HttpStatusCode.BadRequest:
+                    if (error.response.data.errors.lenght) {
+                        customError.message = error.response.data.errors[0];
+                    }
+                    break;
+                case HttpStatusCode.Forbidden:
+                    this.redirectTo('/login')
+                    break;
+                case HttpStatusCode.NotFound:
+                    this.redirectTo('/notfound')
                     break;
                 default:
-                    return Promise.reject(error)
+                    break;
             }
         }
-        return Promise.reject(error)
+        else {
+            customError.message = NetworkError;
+        }
+        return Promise.reject(customError)
     }
 
-    redirectTo = (document, path) => {
+    redirectTo = (path) => {
         document.location = path
     }
 
     get(url) {
-        return this.service.get(url);
+        return this.client.get(url);
     }
 
     post(url, payload) {
-        return this.service.post(url, payload);
+        return this.client.post(url, payload);
     }
 
     put(url, payload) {
-        return this.service.put(url, payload);
+        return this.client.put(url, payload);
     }
 }
 
