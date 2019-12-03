4a40a1c521c6ede2e55e9cafc6998ab2fba17651
saqib abbas <saqib4cs@gmail.com>
2019-12-03 19:32:30 +0500

    feat

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -3,8 +3,6 @@ import './index.css';
 import AppRouter from '../../routes';
 
 class App extends React.Component {
-    componentDidMount() {}
-
     render() {
         return <AppRouter />;
     }
