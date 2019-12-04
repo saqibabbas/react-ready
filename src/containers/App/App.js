94b84abd612d7a27a965eba3e7e33c906d9661b5
saqib abbas <saqib4cs@gmail.com>
2019-12-04 19:09:27 +0500

    build test

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './index.css';
 import AppRouter from '../../routes';
-
+import test from '';
 class App extends React.Component {
+    componentDidMount() {}
 
     render() {
         return <AppRouter />;
